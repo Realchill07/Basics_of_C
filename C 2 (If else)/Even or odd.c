c0c7077c3af453e1001c0406246aaff093403ef8
Lakshya Lakhotia <lakshyalakhotia2@gmail.com>
2025-08-11 02:18:50 +0530

    Fourth commit

--- C 2 (If else)/Even or odd.c
+++ C 2 (If else)/Even or odd.c
@@ -3,7 +3,7 @@ int main(){
     printf("Input a number : ");
     int x;
     scanf("%d",&x);
-    if (x%2 == 0)
+    if (x % 2 == 0)
     printf("%d is even",x);
     else
     printf("%d is odd",x);
